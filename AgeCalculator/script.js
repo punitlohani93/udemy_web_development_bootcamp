5837d3aa827eaf3c8aed681393bdb4544a744b32
punitlohani93 <punit_lohani@yahoo.com>
2017-05-26 02:58:58 -0400

    Finish simple JS exercises.

---
+++ AgeCalculator/script.js
@@ -0,0 +1,2 @@
+var age = prompt("Enter your age");
+alert(age+" years is roughly "+(age* 365 + age/4)+" days.");
---
+++ JSStalkerExercise/script.js
@@ -0,0 +1,5 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("Hey "+firstName+", what is your last name?");
+let age = prompt("Hey "+firstName+", what is your age?");
+console.log("Your full name is "+firstName+" "+lastName+".");
+console.log("You are "+age+" years old.")
